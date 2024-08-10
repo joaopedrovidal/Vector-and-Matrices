2978029780c0113df7c808a37ae11cefef1e6011
John Doe <ppvidalsantos@gmail.com>
2024-08-10 19:56:11 -0300

    Two commit tasks

--- Atividade vetor 5.cpp
+++ Atividade vetor 5.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int vet1 [10];
+int main() {
+    int vet1[10];
     int soma = 0;
     int multiplica = 0;
-    for(int i = 0; i < 10; i++){
+    for (int i = 0; i < 10; i++) {
         cout << "Digite um numero inteiro: " << endl;
         cin >> vet1[i];
-        }
+    }
 
-    int vet2 [10];
+    int vet2[10];
     for (int i = 0; i < 10; i++) {
-        if(i % 2 == 0) {
+        if (i % 2 == 0) {
             vet2[i] = vet1[i] * 5;
-        }else {
-            vet2[i] = vet1 [i] + 5;
+        } else {
+            vet2[i] = vet1[i] + 5;
         }
-    cout << "O conteudo do vetor 1 eh: ";
-        for (int i = 0; i <10; i++) {
+        cout << "O conteudo do vetor 1 eh: ";
+        for (int i = 0; i < 10; i++) {
         }
-        cout << vet1 [i] <<  " ";
+        cout << vet1[i] << " ";
         cout << endl;
     }
     cout << "O conteudo do vetor 2 eh: ";
@@ -31,5 +31,3 @@ int main(){
 
     return 0;
 }
-
-
